8e9f9fe705c35c6bbef1c4d3aac27842510ab5ba
Zoe Weinstein <mxzweinstein@gmail.com>
2021-11-17 20:11:46 -0700

    random generator won't return any duplicate colors

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,62 @@
 //utility functions
-function addSaveDiv(counter, countdown){
+//function to check if a color is in the given array and to return how many times it is
+function numberInArray(hexArray, color){
+  let occurances = 0;
+  hexArray.forEach(function(element){
+    if(element === color){
+      occurances++;
+    }
+  });
+  return occurances
+}
 
+//function to create a div for the saved colors and a number displayed in the div
+function addSaveDiv(counter, countdown){
   let divName = "<div class='col-md-1' id='saveColor" + counter +"'>" + countdown + ".</div>";
   return divName;
 }
+//function to create a div with class "row" and a dynamic id
 function addRow(counter){
   let divName = "<div class='row' id='saveRow" + counter + "'></div>";
   return divName;
 }
+//function to create a div to display the current colors
 function addDiv(counter){
   const displayNumber = counter + 1;
   let divName = "<div class='col-md-3' id='displayColor" + counter +"'>" + displayNumber + ".</div>";
   return divName;  
 }
+
+//function to create an id
 function colorID(counter){
   return "#displayColor" + counter;
 }
+//function to create an id
 function savedID(counter){
   return "#saveColor" + counter;
 }
+
 //business logic
+//creates an array of random hex numbers with no repeating values
 function randomHex(number){
   let hexArray = [];
+  //let numberInArray = 0;
   for(let i = 0; i < number; i++){
     let randomColor = Math.floor(Math.random()*16777215).toString(16);
-    hexArray.push("#" + randomColor);
+    occurances = numberInArray(hexArray, randomColor);
+    //check if the newest color is already in the array
+    while(occurances > 1){
+      randomColor = Math.floor(Math.random()*16777215).toString(16);
+      occurances = numberInArray(hexArray, randomColor);
+    }
+    
+    hexArray.push(randomColor);
   }
-  return hexArray;
+  const symbolArray = hexArray.map(function(color){
+      return "#" + color;
+  });
+  return symbolArray;
 }
-
-
-
 
 
 //UI logice
@@ -42,10 +68,12 @@ $(document).ready(function(){
   let rowTracker = 0;
   let savedDisplay = false;
 
+  //display the current color of the colorpicker to div
   $("#colorPick").on("input", function(){
     let currentColor = $("#colorPick").val();
     $(currentColorID).css("background-color", currentColor);
   });
+  //save the color of the color picker to the next div
   $("#saveButton").click(function(event){
     if(counter <=11){
       let currentColor = $("#colorPick").val();
@@ -69,6 +97,7 @@ $(document).ready(function(){
     };
     event.preventDefault();
   });
+  //take the saved div colors and make new divs with those colors in a new row
   $("#paletteButton").click(function(event){
     colorArray.sort();
     let newRow = addRow(rowTracker);
@@ -95,9 +124,11 @@ $(document).ready(function(){
       oldColorID = colorID(counter);
       rowTracker++;
       $("#savedContainer").show();
+      savedDisplay = true;
     }
     event.preventDefault();
   });
+  //toggle the saved colors
   $("#showSavedButton").click(function(){
     if(savedDisplay){
       $("#savedContainer").hide();
@@ -108,11 +139,10 @@ $(document).ready(function(){
     }
   });
 
-  $("#randomButton").click(function(){
-    colorArray = randomHex(12);
-    for(let i = 0; i < colorArray.length; i++){
-      currentColorID = colorID(i);
-      $(currentColorID).css("background-color", colorArray[i]);
-    }
+  $("#randomButton").click(function(event){
+    colorAmount = parseInt($("#randomAmount").val());
+    colorArray = randomHex(colorAmount);
+    $("#paletteButton").click();
+    event.preventDefault();
   });
 }); 
